a6f55fb32cde9d703ef04be11c246f809534a6aa
Atul Nath <atulnath2909@gmail.com>
2025-09-05 21:25:28 +0530

    Create Footer.js

---
+++ day21/frontend/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="social-links">
+        {/* Add your actual links here */}
+        <a href="#" aria-label="GitHub">GH</a>
+        <a href="#" aria-label="LinkedIn">LI</a>
+        <a href="#" aria-label="Twitter">TW</a>
+      </div>
+      <p>&copy; 2025 Vipin Tomar. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
